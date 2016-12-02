97beff418ddc4957ef0518bc9e3035a45eef5605
Connor Coward <connor.coward@gmail.com>
2016-12-02 14:58:41 -0600

    Added RANSAC filtering for detecting collisions

--- analyze.py
+++ analyze.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python
 
+import math
+import random
+import scipy.stats
+
 try:
     import numpy as np
     import cv2
@@ -39,6 +43,9 @@ def scaleOneSided(img):
     img = img.astype(np.uint8)
     return img
 
+def euclideanDistance(p1, p2):
+    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+
 class trajectory:
     def __init__(self, points = []):
         self.points = points
@@ -47,25 +54,82 @@ class trajectory:
     def movement(self):
         return ((self.points[-1][0] - self.points[-2][0])**2 + (self.points[-1][1] - self.points[-2][1])**2)**0.5
 
+    def distance(self):
+        prevPoint = self.points[0]
+        distance = 0
+        for point in self.points[1:]:
+            distance = distance + euclideanDistance(prevPoint, point)
+            prevPoint = point
+        return distance
+
+    def splitLinear(self):
+        newTrajectories = []
+        originalSize = len(self.points)
+        while len(self.points) > originalSize * 0.1 and len(self.points) > 4:
+            bestNumInliers = 0
+            bestInlierSet = []
+            for _ in range(1000):
+                p1, p2 = random.sample(self.points, 2)
+                if p1 == p2:
+                    continue
+
+                inlierSet = []
+                if p1[0] == p2[0]:
+                    for p in self.points:
+                        d = p[0] - p1[0]
+                        if d < 4.0:
+                            inlierSet.append(p)
+
+                else:
+                    m = (p1[1] - p2[1]) / (p1[0] - p2[0])
+                    b = p1[1] - m * p1[0]
+                    for p in self.points:
+                        d = math.fabs(m*p[0] - p[1] + b) / math.sqrt(m*m + 1)
+                        if d < 5.0:
+                            inlierSet.append(p)
+
+                if len(inlierSet) > bestNumInliers:
+                    bestNumInliers = len(inlierSet)
+                    bestInlierSet = inlierSet
+
+            inlierSet = bestInlierSet
+            print "INLIER SET: ", inlierSet
+            for _ in range(100):
+                m, b, r, p, e = scipy.stats.linregress([point[:2] for point in inlierSet])
+                inlierSet = []
+                for p in self.points:
+                    d = math.fabs(m*p[0] - p[1] + b) / math.sqrt(m*m + 1)
+                    if d < 4.0:
+                        inlierSet.append(p)
+
+            color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            for p in inlierSet:
+                self.points.remove(p)
+                cv2.circle(disp, p[:2], 3, color)
+
+            newTrajectories.append(trajectory(inlierSet))
+
+        return newTrajectories
+
+    def averageVelocity(self):
+        return distance / (len(self.points) - 1)
+
     def getCost(self, c):
+        dX = self.points
+
         dX = (self.points[-1][0] - self.points[-2][0])
         dY = (self.points[-1][1] - self.points[-2][1])
-        print dX
-        print dY
 
-        targetX = self.points[-1][0] + dX
-        targetY = self.points[-1][1] + dY
-        targetY_reflected = self.points[-1][1] - dY
+        targetX = int(round(self.points[-1][0] + dX))
+        targetY = int(round(self.points[-1][1] + dY))
 
-        cv2.circle(disp, self.points[-2], 4, (255, 0, 0))
-        cv2.circle(disp, self.points[-1], 4, (0, 255, 0))
+        cv2.circle(disp, self.points[-2][:2], 4, (255, 0, 0))
+        cv2.circle(disp, self.points[-1][:2], 4, (0, 255, 0))
         cv2.circle(disp, (targetX, targetY), 4, (0, 0, 255))
-        cv2.circle(disp, (targetX, targetY_reflected), 4, (0, 0, 255))
 
         cost = ((c[0] - targetX)**2 + (c[1] - targetY)**2)**0.5
-        cost_reflected = ((c[0] - targetX)**2 + (c[1] - targetY_reflected)**2)**0.5
 
-        return min(cost, cost_reflected)
+        return cost
 
 
 cap = cv2.VideoCapture('../Video/ti.mov') 
@@ -136,7 +200,7 @@ while cap.isOpened():
 
         diff = scaleOneSided(Gmag)
 
-        ret, track = cv2.threshold(diff, 128, 256, cv2.THRESH_BINARY)
+        ret, track = cv2.threshold(diff, 100, 256, cv2.THRESH_BINARY)
         kernel = np.ones((3,3), np.uint8)
         kernelOpen = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]], dtype=np.uint8)
         track = cv2.morphologyEx(track, cv2.MORPH_CLOSE, kernel)
@@ -163,7 +227,7 @@ while cap.isOpened():
             if cv2.contourArea(c) >= 10:
                 m = cv2.moments(c)
                 center = int(m['m10'] / m['m00']), int(m['m01'] / m['m00'])
-                centers.append(center)
+                centers.append((center[0], center[1], frameIndex))
                 cv2.circle(disp, center, 3, (0, 0, 255))
       
         bestTrajectories = dict()
@@ -190,19 +254,35 @@ while cap.isOpened():
                 print "BEST COST: ", bestCost
                 mov = traj.movement()
                 print "MOVEMENT: ", mov
-                if bestTrajectories[bestCenter] == traj and mov and bestCost < 2 * mov + 5:
-                    cv2.putText(disp, str(bestCost), bestCenter, cv2.FONT_HERSHEY_PLAIN, 0.6, (0, 0, 0))
-                    traj.points.append(bestCenter)
+                cv2.putText(disp, str(bestCost), bestCenter[:2], cv2.FONT_HERSHEY_PLAIN, 0.86, (0, 0, 0))
+                if bestTrajectories[bestCenter] == traj and mov and bestCost < 1.5 * mov + 2:
+                    traj.points.append((bestCenter[0], bestCenter[1], frameIndex))
                     centers.remove(bestCenter)
                 else:
                     traj.counter = traj.counter + 1
                     if traj.counter > 4:
+                        splitTrajectories = traj.splitLinear()                     
+                        if splitTrajectories:
+                            for linearTraj1 in splitTrajectories:
+                                for linearTraj2 in splitTrajectories:
+                                    if linearTraj2 == linearTraj1:
+                                        continue
+                                    start = linearTraj1.points[0]
+                                    end = linearTraj2.points[-1]
+                                    print "START: ", start
+                                    print "END: ", end
+                                    timeDistance = start[2] - end[2]
+                                    print "TIME DIST: ", timeDistance
+                                    physicalDistance = euclideanDistance(end[:2], start[:2])
+                                    print "PHYSICAL DIST: ", physicalDistance
+                                    if timeDistance > -1 and timeDistance < 3 and physicalDistance < 10:
+                                        print "FOUND!!"
+                                        cv2.circle(disp, start[:2], 7, (255, 0, 255))
                         trajectories.remove(traj)
 
 
         print "TRAJECTORIES"
         print trajectories
-
 
 
         bestCenters = dict()
@@ -211,7 +291,7 @@ while cap.isOpened():
                 bestDistance = ((center[0] - prevCenters[0][0])**2 + (center[1] - prevCenters[0][1])**2)**0.5
                 bestCenter = prevCenters[0]
                 for prevCenter in prevCenters:
-                    cv2.circle(disp, prevCenter, 3, (255, 0, 0))
+                    cv2.circle(disp, prevCenter[:2], 3, (255, 0, 0))
                     distance = ((center[0] - prevCenter[0])**2 + (center[1] - prevCenter[1])**2)**0.5
                     if distance < bestDistance:
                         bestDistance = distance
@@ -245,7 +325,7 @@ while cap.isOpened():
         print matches
         for center in matches:
             prevCenter = matches[center]
-            cv2.line(disp, center, matches[center], (255, 0, 255))
+            #cv2.line(disp, center, matches[center], (255, 0, 255))
             trajectories.append(trajectory([prevCenter, center]))
             centers.remove(center)
 
@@ -253,9 +333,11 @@ while cap.isOpened():
         print centers
 
         for traj in trajectories:
-            prevCenter = traj.points[0]
+            if len(traj.points) < 5:
+                continue
+            prevCenter = traj.points[0][:2]
             for center in traj.points[1:]:
-                cv2.line(disp, prevCenter, center, (255, 0, 255), 1)
+                cv2.line(disp, prevCenter[:2], center[:2], (255, 0, 255), 1)
                 prevCenter = center
 
         out.write(disp)
